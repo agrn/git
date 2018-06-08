29663c3f30e7cd8598f885515f70737019580871
Alban Gruin <alban.gruin@gmail.com>
2018-06-08 19:12:31 +0200

    rebase -i: rewrite setup_reflog_action() in C
    This rewrites setup_reflog_action() from shell to C.
    
    A new command is added to rebase--helper.c, “setup-reflog”, as such as a
    new flag, “verbose”, to silence the output of the checkout operation
    called by setup_reflog_action().
    
    The shell version is then stripped in favour of a call to the helper. As
    $GIT_REFLOG_ACTION is not longer set at the first call of
    checkout_onto(), a call to comment_for_reflog() is added at the
    beginning of this function.
    
    Signed-off-by: Alban Gruin <alban.gruin@gmail.com>

--- sequencer.c
+++ sequencer.c
@@ -3131,6 +3131,37 @@ static const char *reflog_message(struct replay_opts *opts,
 	return buf.buf;
 }
 
+static int checkout_base_commit(struct replay_opts *opts, const char *commit,
+				int verbose, const char *action)
+{
+	struct child_process cmd = CHILD_PROCESS_INIT;
+
+	cmd.git_cmd = 1;
+
+	argv_array_push(&cmd.args, "checkout");
+	argv_array_push(&cmd.args, commit);
+	argv_array_pushf(&cmd.env_array, GIT_REFLOG_ACTION "=%s", action);
+
+	if (verbose)
+		return run_command(&cmd);
+	else
+		return run_command_silent_on_success(&cmd);
+}
+
+int setup_reflog_action(struct replay_opts *opts, const char *commit,
+			int verbose)
+{
+	const char *action;
+
+	if (commit && *commit) {
+		action = reflog_message(opts, "start", "checkout %s", commit);
+		if (checkout_base_commit(opts, commit, verbose, action))
+			die(_("Could not checkout %s"), commit);
+	}
+
+	return 0;
+}
+
 static const char rescheduled_advice[] =
 N_("Could not execute the todo command\n"
 "\n"
