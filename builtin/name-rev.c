4a512c83b0a1f5319758aa066fb145c379cba547
Alban Gruin <alban.gruin@gmail.com>
2019-08-18 00:14:13 +0200

    name-rev: don’t save the commit buffer
    name-rev don’t use commits messages, so let’s turn off
    save_commit_buffer in this command.
    
    Signed-off-by: Alban Gruin <alban.gruin@gmail.com>

--- builtin/name-rev.c
+++ builtin/name-rev.c
@@ -490,6 +490,8 @@ int cmd_name_rev(int argc, const char **argv, const char *prefix)
 		OPT_END(),
 	};
 
+	save_commit_buffer = 0;
+
 	init_commit_rev_name(&rev_names);
 	git_config(git_default_config, NULL);
 	argc = parse_options(argc, argv, prefix, opts, name_rev_usage, 0);
