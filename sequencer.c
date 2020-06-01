a678786aa6dc0bbf310dd481346a057919f416f3
Alban Gruin <alban.gruin@gmail.com>
2020-06-01 17:33:26 +0200

    sequencer: use the "octopus" merge strategy without forking
    This teaches the sequencer to invoke the "octopus" strategy with a
    function call instead of forking.
    
    Signed-off-by: Alban Gruin <alban.gruin@gmail.com>

--- sequencer.c
+++ sequencer.c
@@ -1927,6 +1927,9 @@ static int do_pick_commit(struct repository *r,
 		if (!strcmp(opts->strategy, "resolve")) {
 			repo_read_index(r);
 			res |= merge_resolve(r, common, oid_to_hex(&head), remotes);
+		} else if (!strcmp(opts->strategy, "octopus")) {
+			repo_read_index(r);
+			res |= merge_octopus(r, common, oid_to_hex(&head), remotes);
 		} else
 			res |= try_merge_command(r, opts->strategy,
 						 opts->xopts_nr, (const char **)opts->xopts,
